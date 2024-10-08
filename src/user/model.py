57ee4389e165f3311946b070c766b23eac0a3529
Nurlan-Aliev <nualiev612@gmail.com>
2024-10-08 14:06:35 +0400

    make username unique

--- src/user/model.py
+++ src/user/model.py
@@ -8,11 +8,12 @@ class UserModel(Base):
     name: Mapped[str] = mapped_column(nullable=False)
     surname: Mapped[str] = mapped_column(nullable=False)
     age: Mapped[int]
-    username: Mapped[str] = mapped_column(String(25))
+    username: Mapped[str] = mapped_column(String(25), unique=True)
     email_address: Mapped[str] = mapped_column(nullable=False)
     password: Mapped[bytes] = mapped_column(LargeBinary, nullable=False)
     active: Mapped[bool] = mapped_column(default=True)
     paid: Mapped[bool] = mapped_column(default=False)
+
     create_at: Mapped[datetime] = mapped_column(
         DateTime(timezone=True),
         default=lambda: datetime.datetime.now(datetime.timezone.utc),
